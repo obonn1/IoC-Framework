49d8d3bf815b54f60863f90b1ea30a6e841a26bb
Omar Bonnet <obonnet@techsola.com>
2024-09-05 16:47:43 -0400

    Add project files.

---
+++ Core/Container.cs
@@ -0,0 +1,36 @@
+﻿namespace IoC_Framework.Core;
+
+public class Container
+{
+    private readonly Dictionary<Type, Type> registrations = new();
+
+    public void Register<TInterface, TImplementation>()
+    {
+        registrations[typeof(TInterface)] = typeof(TImplementation);
+    }
+
+    public TInterface Resolve<TInterface>()
+    {
+        return (TInterface)Resolve(typeof(TInterface));
+    }
+
+    private object Resolve(Type interfaceType)
+    {
+        if (!registrations.ContainsKey(interfaceType))
+            throw new Exception($"Type {interfaceType.Name} not registered");
+
+        var implementationType = registrations[interfaceType];
+        var constructor = implementationType.GetConstructors().First();
+
+        var parameters = constructor.GetParameters()
+            .Select(parameter => Resolve(parameter.ParameterType))
+            .ToArray();
+
+        var instance = Activator.CreateInstance(implementationType, parameters);
+
+        if (instance is null)
+            throw new Exception($"Failed to create instance of {implementationType.Name}");
+
+        return instance;
+    }
+}
---
+++ SourceGeneration/SourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace IoC_Framework.SourceGeneration;
+
+[Generator]
+public class SourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context) { }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var source = 
+        """
+            // Auto-generated code for DI
+            public static class GeneratedContainer
+            {
+                public static void Initialize() { /*...*/ }
+            }
+        """;
+
+        context.AddSource("GeneratedContainer.cs", source);
+    }
+}
