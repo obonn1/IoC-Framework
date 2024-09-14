477192e60fd4899f1c3772c8399c77876a293a3c
Omar Bonnet <obonnet@techsola.com>
2024-09-14 14:06:42 -0400

    Establish Initial testing

---
+++ IoC_Framework.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using IoC_Framework.Core;
+using Shouldly;
+
+[assembly: FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+
+namespace IoC_Framework.Tests;
+
+public interface IService { }
+public class ServiceImplementation : IService { }
+public class AnotherService(IService service)
+{
+    public IService Service { get; } = service;
+}
+
+public class Tests
+{
+    [TestFixture]
+    public class ContainerTests
+    {
+        private readonly Container container = new();
+
+        [Test]
+        public void Register_and_resolve_interfe_should_return_correct_implementation()
+        {
+            // Arrange
+            container.Register<IService, ServiceImplementation>();
+
+            // Act
+            var result = container.Resolve<IService>();
+
+            // Assert
+            result.ShouldBeOfType<ServiceImplementation>();
+        }
+
+        [Test]
+        public void Resolving_unregistered_interface_should_throw_exception()
+        {
+            // Act & Assert
+            var exception = Should.Throw<Exception>(() => container.Resolve<IService>());
+            exception.Message.ShouldBe("Type IService not registered");
+        }
+    }
+}
